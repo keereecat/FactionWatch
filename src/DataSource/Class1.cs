40d97e883022b33990d58f76a147d5a5738f1ff2
Keeree Lewah <spots@keer.ee>
2017-12-14 20:21:53 -0800

    Beginning of structure
    - Testing will be done with NUnit.
    - Repository model for persistance.
    - Expect an IoC container shortly.
    - DataSource is for incoming data, which will be event driven.
    - FactionWatch is the startup project
      - FactionWatch.Program is the actual main.
      - Try to keep as small as possible
        - Unlike this commit message. :V
    - Expect class renaming going forward!
      - All projects currently empty.
      - Just trying to get things started.

---
+++ src/DataSource/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataSource
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/FactionWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactionWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
